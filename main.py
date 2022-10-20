7703477a832138e289a29b264b3ce34d0b4afd35
Pavel Ershov <40699895+PavelErsh@users.noreply.github.com>
2022-10-21 01:53:27 +0200

    update phisics

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from curses.ascii import NUL
 from tkinter import SEL
 import arcade
 import math
@@ -5,20 +6,24 @@ import math
 SCREEN_WIDHT = 500
 SCEEN_HEIGHT = 500
 TITLE = "GUN"
+GUN_MAX_RANGLE = 126
+GUN_MAX_LANGLE = 0
+GRAWITATION = 0.2
 
 class Gun(arcade.Sprite):
     def __init__(self):
         super().__init__("aa.png",0.1)
         self.center_x = SCREEN_WIDHT/2
         self.center_y = 100
-        self.angle= 0
+        self.angle= GUN_MAX_LANGLE
        
     def update(self):
-        if self.angle <= 0:
-            self.angle = 0
-        if self.angle >= 126:
-            self.angle = 126
+        if self.angle <= GUN_MAX_LANGLE:
+            self.angle = GUN_MAX_LANGLE
+        if self.angle >= GUN_MAX_RANGLE:
+            self.angle = GUN_MAX_RANGLE
         self.angle+=self.change_angle
+        print(self.angle)
     
 class Bullet(arcade.Sprite):
     def __init__(self, gun):
@@ -27,9 +32,13 @@ class Bullet(arcade.Sprite):
         self.bottom = self.gun.top
         self.center_x = self.gun.center_x
         self.change_y = 2
+
+        self.change_x = self.gun.angle - 61
     def update(self):
        self.center_y+= self.change_y
-       self.change_y-=0.01
+       self.change_y-=GRAWITATION
+       self.center_x -= self.change_x / 5
+       
 
 class Game(arcade.Window):
     def __init__(self, widht, height, title):
@@ -50,7 +59,7 @@ class Game(arcade.Window):
             
     def on_key_release(self, key, modifiers):
         if key == arcade.key.LEFT or key == arcade.key.RIGHT:
-            self.gun.change_angle = 0
+            self.gun.change_angle = NUL
         
     def on_draw(self):
         self.clear()
